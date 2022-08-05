7f9e9665b75e032eb568a0b8e2687146aa17bd70
linya <>
2022-08-05 20:48:44 +0800

    init

---
+++ src/test/kotlin/RunMirai.kt
@@ -0,0 +1,20 @@
+package com.linya.mirai
+
+import net.mamoe.mirai.alsoLogin
+import net.mamoe.mirai.console.MiraiConsole
+import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
+import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
+import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
+
+suspend fun main() {
+    MiraiConsoleTerminalLoader.startAsDaemon()
+
+    PluginMain.load()
+    PluginMain.enable()
+
+    val bot = MiraiConsole.addBot(3304895821, "wanwan168168..") {
+        fileBasedDeviceInfo()
+    }.alsoLogin()
+
+    MiraiConsole.job.join()
+}
